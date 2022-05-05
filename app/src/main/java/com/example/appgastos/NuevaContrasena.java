cbebb429072cfe623825e7b70c514ef62fe46f78
Jorge domínguez <herrerajorge725@gmail.com>
2022-05-05 18:10:43 -0500

    Recuperar contraseña (interfaces)

---
+++ app/src/main/java/com/example/appgastos/NuevaContrasena.java
@@ -0,0 +1,14 @@
+package com.example.appgastos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NuevaContrasena extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nueva_contrasena);
+    }
+}
