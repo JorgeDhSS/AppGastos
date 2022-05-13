1756fa2c7aa8c139031b69d9b10140abede16a12
Jorge domínguez <herrerajorge725@gmail.com>
2022-05-13 00:36:11 -0500

    Solución de errores Firebase

--- app/src/main/java/com/example/appgastos/Login.java
+++ app/src/main/java/com/example/appgastos/Login.java
@@ -15,6 +15,7 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -32,6 +33,7 @@ public class Login extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FirebaseApp.initializeApp(this);
         setContentView(R.layout.activity_login);
 
         Eduser = findViewById(R.id.user);
