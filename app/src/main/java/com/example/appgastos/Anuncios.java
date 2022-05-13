a3d8ad48b12caac68198d77471cf01e704536451
JazmínMonserrat <71732504+JazminMonserrat@users.noreply.github.com>
2022-05-13 12:36:37 -0500

    Add files via upload
    Ingreso, gasto y anuncios

---
+++ app/src/main/java/com/example/appgastos/Anuncios.java
@@ -0,0 +1,14 @@
+package com.example.appgastos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Anuncios extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_anuncios);
+    }
+}
