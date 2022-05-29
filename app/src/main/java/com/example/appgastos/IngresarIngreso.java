8dca27fb42411b24ea64dd7f5484d9524fb9a835
Jazmín Gómez <jaz99violin@hotmail.com>
2022-05-29 15:55:27 -0500

    Corrección de errores de interfaz

--- app/src/main/java/com/example/appgastos/IngresarIngreso.java
+++ app/src/main/java/com/example/appgastos/IngresarIngreso.java
@@ -2,7 +2,15 @@ package com.example.appgastos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
 
 public class IngresarIngreso extends AppCompatActivity {
 
@@ -10,5 +18,55 @@ public class IngresarIngreso extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ingresar_ingreso);
+        Button guardarIngreso = findViewById(R.id.btnGuardarIngreso);
+        this.nuevoIngreso = findViewById(R.id.nuevoIngreso);
+        this.efectivo = findViewById(R.id.efectivo);
+        this.nota = findViewById(R.id.nota);
+        guardarIngreso.setOnClickListener(view -> {
+            this.guardarIngresos();
+        });
+    }
+
+    private EditText nuevoIngreso;
+    private Spinner efectivo;
+    private EditText nota;
+
+    private void guardarIngresos(){
+        if(this.validarNuevoIngreso() && this.validarEfectivo()){
+            HashMap<String, Object> nuevoRegistro = new HashMap<>();
+            String ingreso = this.nuevoIngreso.getText().toString();
+            nuevoRegistro.put("Ingreso", Float.parseFloat(ingreso));
+            nuevoRegistro.put("TipoIngreso", this.efectivo.getSelectedItem());
+            nuevoRegistro.put("Nota", this.nota.getText().toString());
+            nuevoRegistro.put("Fecha", this.generarFecha());
+        }
+    }
+
+    private boolean validarNuevoIngreso(){
+        boolean validacion = false;
+        String ingreso = this.nuevoIngreso.getText().toString();
+        try {
+            if(!ingreso.isEmpty()){
+                Float.parseFloat(ingreso);
+                validacion = true;
+            }
+        }catch (NumberFormatException e){
+        }
+        return validacion;
+    }
+
+    private boolean validarEfectivo(){
+        boolean validacion = false;
+        if(this.efectivo.getSelectedItem()!=null && !this.efectivo.getSelectedItem().equals("Tipo de Ingreso")){
+            validacion = true;
+        }
+        return validacion;
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    private String generarFecha(){
+        SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date date = new Date(System.currentTimeMillis());
+        return formatter.format(date);
     }
 }
--- app/src/main/java/com/example/appgastos/PaginaPrincipal.java
+++ app/src/main/java/com/example/appgastos/PaginaPrincipal.java
@@ -2,7 +2,10 @@ package com.example.appgastos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PaginaPrincipal extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class PaginaPrincipal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pagina_principal);
+        Button btn_ingreso = findViewById(R.id.btn_ingreso);
+        btn_ingreso.setOnClickListener(view -> {
+            this.ingresarIngreso();
+        });
+    }
+
+    private void ingresarIngreso(){
+        Intent ingreso = new Intent(this,IngresarIngreso.class);
+        startActivity(ingreso);
     }
 }
