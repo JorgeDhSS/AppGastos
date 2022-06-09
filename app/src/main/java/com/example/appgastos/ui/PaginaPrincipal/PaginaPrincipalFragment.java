4070909a8f83ebbea31f6abcc23eca287d54f143
Jazmín Gómez <jaz99violin@hotmail.com>
2022-06-09 18:32:53 -0500

    Reporte de gastos y actualización en página principal y menú

--- app/src/main/java/com/example/appgastos/ui/PaginaPrincipal/PaginaPrincipalFragment.java
+++ app/src/main/java/com/example/appgastos/ui/PaginaPrincipal/PaginaPrincipalFragment.java
@@ -6,15 +6,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.appgastos.IngresarIngreso;
+import com.example.appgastos.Login;
+import com.example.appgastos.MainActivity;
 import com.example.appgastos.R;
 import com.example.appgastos.databinding.FragmentPaginaPrincipalBinding;
 import com.google.firebase.auth.FirebaseAuth;
@@ -26,21 +27,28 @@ import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 public class PaginaPrincipalFragment extends Fragment {
-    private PaginaPrincipalViewModel paginaPrincipalViewModel;
     private FragmentPaginaPrincipalBinding binding;
     private FirebaseAuth mAuth = FirebaseAuth.getInstance();
     private DatabaseReference ingresosDB;
-    TextView totalIngresos;
+    private TextView totalIngresos;
+    private TextView totalGastos;
+    private TextView totalSaldo;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        paginaPrincipalViewModel =
-                new ViewModelProvider(this).get(PaginaPrincipalViewModel.class);
+        PaginaPrincipalViewModel paginaPrincipalViewModel = new ViewModelProvider(this).get(PaginaPrincipalViewModel.class);
 
         binding = FragmentPaginaPrincipalBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
         Button btn_ingreso = root.findViewById(R.id.btn_ingreso);
         totalIngresos = root.findViewById(R.id.txtTotalIngresos);
+        totalGastos = root.findViewById(R.id.txtGastos);
+        totalSaldo = root.findViewById(R.id.saldoTotal);
+        ImageView btn_session = root.findViewById(R.id.btn_cerrar);
+        btn_session.setOnClickListener(view -> {
+            Intent activityChangeIntent = new Intent(getActivity(), Login.class);
+            startActivity(activityChangeIntent);
+        });
         this.sumarIngresos();
         btn_ingreso.setOnClickListener(view -> {
             this.ingresarIngreso();
@@ -74,6 +82,7 @@ public class PaginaPrincipalFragment extends Fragment {
                     }
                     totalIngresos.setText(String.valueOf(total));
                 }
+                sumarGastos();
             }
 
             @Override
@@ -81,5 +90,34 @@ public class PaginaPrincipalFragment extends Fragment {
 
             }
         });
+    }
+
+    private void sumarGastos () {
+        this.ingresosDB = FirebaseDatabase.getInstance().getReference();
+
+        Query query = this.ingresosDB.child("Users").child(mAuth.getCurrentUser().getUid()).child("Gastos");
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists()){
+                    float total = 0;
+                    for(DataSnapshot ingreso : snapshot.getChildren()){
+                        total += Float.parseFloat(ingreso.child("amount").getValue().toString());
+                    }
+                    totalGastos.setText(String.valueOf(total));
+                }
+                calcularTotal();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void calcularTotal () {
+        float total = Float.parseFloat(totalIngresos.getText().toString()) - Float.parseFloat(totalGastos.getText().toString());
+        totalSaldo.setText(String.valueOf(total));
     }
 }
